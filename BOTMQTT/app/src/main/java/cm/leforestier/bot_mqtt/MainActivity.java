4365fbe8778a8ae2e04a41424482cf534808cc98
Ruphus-Leforestier <46247273+ruphus-leforestier@users.noreply.github.com>
2019-08-08 18:18:01 +0200

    SQLite-Database Created: - Read and Write access: OK
    Graph:
    - Not successfully implemented

--- BOTMQTT/app/src/main/java/cm/leforestier/bot_mqtt/MainActivity.java
+++ BOTMQTT/app/src/main/java/cm/leforestier/bot_mqtt/MainActivity.java
@@ -1,15 +1,25 @@
 package cm.leforestier.bot_mqtt;
 
+import android.annotation.SuppressLint;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -18,14 +28,20 @@ import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import warning_model.Warning;
 import warning_model.WarningManager;
 
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 // MPAndroidChart Documentation: https://weeklycoding.com/mpandroidchart-documentation/
+// Exemple 12 : https://www.programcreek.com/java-api-examples/?api=com.github.mikephil.charting.formatter.IAxisValueFormatter
     private final String TAG = MainActivity.class.getSimpleName();
 
     private String topicMsg = "strom/bot";
@@ -41,7 +57,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView hour_tv;
     private LineChart graph;
 
+    private EditText tmp_tv;
     List<Entry> entries = new ArrayList<Entry>();
+
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd MMM YYYY");
 
     BOTMQTTDB helper;
     SQLiteDatabase database;
@@ -53,7 +73,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         onInitializedViews();
-        onCreateAndroidClientMQTT();
+
+        //onWriting(v);
+        //onRead();
+        //onCreateAndroidClientMQTT();
     }
 
     private void subscr(String top, int qos){
@@ -127,13 +150,14 @@ public class MainActivity extends AppCompatActivity {
         msg_tv = (TextView) findViewById(R.id.message_id);
         date_tv = (TextView) findViewById(R.id.date_val_id);
         hour_tv = (TextView) findViewById(R.id.hour_val_id);
+        tmp_tv = (EditText) findViewById(R.id.tmp_id);
         graph = (LineChart) findViewById(R.id.diag_id);
-
-        entries.add(new Entry(0.0f, 0.0f)); // add values to an  entry
 
         helper = new BOTMQTTDB(this);
         database = helper.getWritableDatabase();
         Log.d(TAG, "Views are initialized");
+
+        entries.add(new Entry(0.0f, 0.0f)); // add values to an  entry
     }
 
     private void onCreateAndroidClientMQTT(){
@@ -188,5 +212,83 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void onWriting(double val){
+
+        long datetime = System.currentTimeMillis();
+        helper.onInsertValue(val, datetime);
+        Log.d(TAG, "Value: ( "+val+" ) saved in the DB");
+    }
+    private void onRead(){
+        List<Warning> warningList = manager.groupDaily(helper.getWarnings());
+        msg_tv.setText(String.valueOf(warningList.get(0).getValue()));
+        date_tv.setText(warningList.get(0).getDate());
+        hour_tv.setText(warningList.get(0).getTime());
+
+        Log.d(TAG, "LIST CONTENT: "+warningList);
+
+        for (Warning w: warningList) {
+            Log.d(TAG, "id: " + w.get_id() + "  Value: " + w.getValue() + " Date: " + w.getDate() + "  Time: " + w.getTime());
+        }
+
+        graph.
+    }
+    private String convertIncomingTime(long itime){
+        int hour = (int) ((itime / 1000) / 3600);
+        int minuts = (int) (((itime / 1000) / 60) % 60);
+        int seconds = (int) ((itime / 1000) % 60);
+
+        return hour + "h : "+minuts+ "m : "+seconds+"s";
+    }
+
+    public void onCOmmand(View view) {
+        double v = Double.parseDouble(tmp_tv.getText().toString());
+        onWriting(v);
+        onRead();
+    }
+
+    private void onSettingGraph(LineChart chart){
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+        chart.setHighlightPerDragEnabled(true);
+
+        // get the legend (only possible after setting data)
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
+        //xAxis.setTypeface(mTfLight);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(true);
+        xAxis.setTextColor(Color.rgb(255, 192, 56));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setGranularity(1f); // one hour
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return sdf.format(new Date((long) value));
+            }
+        });
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        //leftAxis.setTypeface(mTfLight);
+        leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(170f);
+        leftAxis.setYOffset(-9f);
+        leftAxis.setTextColor(Color.rgb(255, 192, 56));
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
     }
 }
